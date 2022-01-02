731c0256a3d879cdf5b80f130e109328d2e4e67f
Timyu0527 <st970387@gmail.com>
2022-01-03 00:54:20 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class = "container">
+    <Header title = "Hello"/>
+    <Items :items = "items"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header"
+import Items from "./components/Items"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Items,
+  },
+  data(){
+    return {
+      items: []
+    }
+  },
+  created(){
+    this.items = [
+      {
+        id: 1,
+        shop: "welcome",
+        date: "2022/1/2"
+      },
+      {
+        id: 2,
+        shop: "jaklsdf",
+        date: "2022/1/2"
+      },
+      {
+        id: 3,
+        shop: "jaklsdf",
+        date: "2022/1/2"
+      },
+    ]
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+} */
+body {
   margin-top: 60px;
+  font-family: 'Poppins', sans-serif;
+}
+.container{
+  width: 80%;
+  /* margin: 30px; */
+  margin-left: auto;
+  margin-right: auto;
+  overflow: auto;
+  min-height: 100%;
+  border: 3px solid seagreen;
+  padding: 30px;
+  padding-top: 10px;
+  border-radius: 10px;
 }
 </style>
+
---
+++ src/components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button @click = "onClick()" class = "button">{{text}}</button>
+</template>
+
+<script>
+
+export default {
+    name: "Button",
+    props:{
+        text: String,
+    },
+    methods: {
+        onClick(){
+            alert("click");
+        },
+    }
+}
+</script>
+<style scoped>
+.button {
+  background-color: rgb(47, 219, 32); /* Green */
+  margin: 0px;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 10px;
+}
+</style>
