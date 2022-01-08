16bd182e377f7e8b38130cc98c65fce0ae558d41
Timyu0527 <st970387@gmail.com>
2022-01-08 14:19:33 +0800

    add database

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDtrtsJaFh8mQgvlyAxJVG2tm5ugbC0IsI",
+  authDomain: "webproject-da7bc.firebaseapp.com",
+  projectId: "webproject-da7bc",
+  storageBucket: "webproject-da7bc.appspot.com",
+  messagingSenderId: "600994560335",
+  appId: "1:600994560335:web:917abf467eb64d9b3aed3a",
+  measurementId: "G-HYRYSHE9M3"
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "./firebase.js"
 
+import firebase from 'firebase'
+// import "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(firebaseApp)
+export const db = firestroe()
+// db.collection('shop').get().then(r =>{
+//     r.docs.map(doc => {
+//         console.log(doc.data());
+//     });
+// });
+// db.collection
+console.log(db.collection('shop'));
+// const analytics = getAnalytics(app);
 createApp(App).mount('#app')
