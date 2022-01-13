02a4f036bc817a1a1da9fb503d6fbb9f64e8f073
Rager0321 <hp34217419@gmail.com>
2022-01-13 16:48:46 +0800

    fix my bug and merge youwaiting

--- src/main.js
+++ src/main.js
@@ -2,36 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-<<<<<<< HEAD
-import { firebaseConfig } from "./firebase.js"
-
-import firebase from 'firebase'
-=======
 import { getFirestore } from 'firebase/firestore/lite';
 import { firebaseConfig } from "./firebase.js"
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import router from './router'
 // import firebase from 'firebase'
->>>>>>> fe37b3f2a732fe737f328f1b83ca82a3e6a84100
 // import "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
-<<<<<<< HEAD
-const firebaseApp = initializeApp(firebaseConfig)
-// const analytics = getAnalytics(firebaseApp)
-export const db = firestroe()
-// db.collection('shop').get().then(r =>{
-//     r.docs.map(doc => {
-//         console.log(doc.data());
-//     });
-// });
-// db.collection
-console.log(db.collection('shop'));
-// const analytics = getAnalytics(app);
-createApp(App).mount('#app')
-=======
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 export const auth = getAuth();
@@ -54,4 +34,3 @@ createApp(App).use(router).mount('#app');
 // register("test@test.com", "testtest");
 // console.log(login("test@test.com", "testtest"));
 export { login, register };
->>>>>>> fe37b3f2a732fe737f328f1b83ca82a3e6a84100
